d942e9568e806f7e7ae0c665fbb53b153b3bca2d
amyhuang95 <hshuang@ucsd.edu>
2024-07-10 21:40:11 -0700

    Update user model

--- calorie_tracker/tracker/urls.py
+++ calorie_tracker/tracker/urls.py
@@ -5,4 +5,7 @@ urlpatterns= [
     path("", views.index, name="index"),
     path("food-search/", views.food_search, name="food_search"),
     path("food-detail/<str:food_id>", views.food_detail, name="food_detail"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
 ]
