7dd1f0229588bc0ae03bd6dfd7b757e4e302f2a5
amyhuang95 <hshuang@ucsd.edu>
2024-07-24 21:45:17 -0700

    Removed nutrition info  model and added comments

--- calorie_tracker/tracker/models.py
+++ calorie_tracker/tracker/models.py
@@ -1,8 +1,21 @@
+'''
+Data models for food entry.
+'''
 from django.db import models
 from django.contrib.auth.models import User
 
 
 class FoodEntry(models.Model):
+    """
+    Food entry data model. 
+    user: default user object
+    food_id: food id in Fatsecret API
+    name: food name
+    calories: amount of calories in the food
+    protein: amount of protein in the food
+    carbohydrates: amount of carbohydrates in the food
+    date: creation date
+    """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     food_id = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
@@ -13,12 +26,5 @@ class FoodEntry(models.Model):
     date = models.DateField(auto_now_add=True)
 
     def __str__(self):
+        """Display the entry as a string"""
         return f"{self.name} - {self.calories} calories"
-
-class NutritionalInformation(models.Model):
-    food_entry = models.ForeignKey(FoodEntry, related_name='nutritional_info', on_delete=models.CASCADE)
-    nutrient_name = models.CharField(max_length=255)
-    amount = models.DecimalField(max_digits=6, decimal_places=2)
-
-    def __str__(self):
-        return f"{self.nutrient_name} - {self.amount}"
