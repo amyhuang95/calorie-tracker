eaeffb5416b4e12bf0cb141f5908bdc31d641130
amyhuang95 <hshuang@ucsd.edu>
2024-07-24 22:02:43 -0700

    Removed nutrition info  model and added comments

--- calorie_tracker/tracker/admin.py
+++ calorie_tracker/tracker/admin.py
@@ -1,12 +1,11 @@
+"""
+Register models in admin dashboard.
+"""
+
 from django.contrib import admin
-from .models import FoodEntry, NutritionalInformation
+from .models import *
 
 @admin.register(FoodEntry)
 class FoodEntryAdmin(admin.ModelAdmin):
     list_display = ('name', 'calories', 'date')
     search_fields = ('name',)
-
-@admin.register(NutritionalInformation)
-class NutritionalInformationAdmin(admin.ModelAdmin):
-    list_display = ('food_entry', 'nutrient_name', 'amount')
-    search_fields = ('nutrient_name',)
